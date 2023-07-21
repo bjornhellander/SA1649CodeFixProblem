f0a070ab4b9a56e384e775657f0e629d9337e4ea
Björn Hellander <bjorn.hellander@gmail.com>
2023-07-21 15:48:22 +0200

    Add project files.

---
+++ ClassLibrary1/File2.cs
@@ -0,0 +1,10 @@
+﻿// <copyright file="File2.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace ClassLibrary1
+{
+    internal class Class2
+    {
+    }
+}
---
+++ ClassLibrary3/File3.cs
@@ -0,0 +1,10 @@
+﻿// <copyright file="File3.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace ClassLibrary3
+{
+    public class Class3
+    {
+    }
+}
